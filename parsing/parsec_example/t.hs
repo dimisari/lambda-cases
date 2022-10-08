267fb9d811067ad205c419c264fd76d096eb33d9
Gnostis <saridakisbitos@gmail.com>
2022-10-08 20:43:06 +0300

    No! No! I'm not an automated comment! It's all lies!

--- parsing/parsec_example/t.hs
+++ parsing/parsec_example/t.hs
@@ -1,15 +1,23 @@
 import Text.Parsec
-import Text.Parsec.String
-import Text.ParserCombinators.Parsec.Number
+  ( parse, ParseError, (<|>), try
+  , space, char, letter, string, eof
+  , many, many1)
+import Text.Parsec.String (Parser)
+import Text.ParserCombinators.Parsec.Number (int)
 
 -- Helpers
 
-[ letters, spaces1 ] = map many1 [ letter, space ] :: [ Parser String ]
-spaces_then = ( spaces1 *> ) :: Parser a -> Parser a
-(.>) = flip (.) :: (a -> b) -> (b -> c) -> a -> c
+[ letters, spaces1 ] =
+  map many1 [ letter, space ]
+  :: [ Parser String ]
 
-parse_with :: Parser a -> String -> Either ParseError a
-parse_with = flip parse "" 
+spaces_then =
+    ( spaces1 *> )
+    :: Parser a -> Parser a
+
+(.>) =
+  flip (.)
+  :: (a -> b) -> (b -> c) -> a -> c
 
 -- Statement parsers
 
@@ -22,18 +30,20 @@ data Statement =
   , Assign <$> letters <*> ( spaces_then ( char '=' ) *> spaces_then int )
   , Add    <$> letters <*> ( spaces_then ( string "+=" ) *> spaces_then letters )
   , While  <$> ( string "while" *> spaces_then letters ) <*> positive_then_while_body 
-  , try printp <|> try assignment <|> try addition <|> while
-  ] :: [ Parser Statement ]
+  , try printp <|> try assignment <|> try addition <|> while ]
+  :: [ Parser Statement ]
 
 -- Statements parsers
 
 [ statements, positive_then_while_body ] = 
   [ many ( statement <* spaces1 )
-  , spaces_then ( string "positive" ) *>
-    spaces_then ( char '{' ) *> spaces_then statements <* spaces_then ( char '}' )
-  ] :: [ Parser [ Statement ] ]
+  , spaces_then ( string "positive {" ) *> spaces_then statements <* char '}' ]
+  :: [ Parser [ Statement ] ]
 
-parse_string :: String -> Either ParseError [ Statement ]
-parse_string = parse ( statements <* eof ) "temp.in"
+parse_string =
+  parse ( statements <* eof ) "temp.in"
+  :: String -> Either ParseError [ Statement ]
 
-main = readFile "temp.in" >>= parse_string .> print
+main =
+  readFile "temp.in" >>= parse_string .> print
+  :: IO ()
