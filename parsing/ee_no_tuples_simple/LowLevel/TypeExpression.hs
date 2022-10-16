320b2a14abb3601aa4b2160c1bba4b9809522db0
Gnostis <saridakisbitos@gmail.com>
2022-10-16 20:15:30 +0300

    Just Saving

--- parsing/ee_no_tuples_simple/LowLevel/TypeExpression.hs
+++ parsing/ee_no_tuples_simple/LowLevel/TypeExpression.hs
@@ -1,33 +1,35 @@
+{-# LANGUAGE LambdaCase #-}
+
 module LowLevel.TypeExpression where
 
-import Prelude (Eq, Show, pure, (<*), (*>), (<*>), (<$>))
-import Text.Parsec ( (<|>), try, char, string, sepBy1)
+import Prelude ( Eq, show, Show, pure, (<*), (*>), (<*>), (<$>), (++) )
+import Text.Parsec ( (<|>), try,  string, sepBy1)
 import Text.Parsec.String (Parser)
 
 -- TypeExpression
 
-data TypeExpression = FinalType TupleOrIntType | FunctionType TupleOrIntType TypeExpression
-  deriving (Eq, Show)
+data TypeExpression =
+  Result TupleOrIntType | Function TupleOrIntType TypeExpression
+  deriving (Eq)
 
-type_expression_p = try function_type_p <|> tuple_or_int_expr_p
-  :: Parser TypeExpression
+instance Show TypeExpression where
+  show = \case
+    Result rt -> show rt
+    Function toit te -> show toit ++ " right_arrow " ++ show te
 
-tuple_or_int_expr_p = FinalType <$> tuple_or_int_p
-  :: Parser TypeExpression
-
-function_type_p = FunctionType <$> tuple_or_int_p <*> (string " -> " *> type_expression_p)
-  :: Parser TypeExpression
+[ type_expression_p, result_p, function_type_p ] =
+  [ try function_type_p <|> result_p
+  , Result <$> tuple_or_int_p
+  , Function <$> tuple_or_int_p <*> (string " -> " *> type_expression_p) ]
+  :: [ Parser TypeExpression ]
 
 -- TupleOrIntType
 
 data TupleOrIntType = TupleType [ TypeExpression ] | IntType
   deriving (Eq, Show)
 
-tuple_or_int_p = tuple_type_p <|> int_type_p
-  :: Parser TupleOrIntType
-
-tuple_type_p = TupleType <$> (char '(' *> sepBy1 type_expression_p (string ", ") <* char ')')
-  :: Parser TupleOrIntType
-
-int_type_p = string "Int" *> pure IntType
-  :: Parser TupleOrIntType
+[ tuple_or_int_p, tuple_type_p, int_type_p ] =
+  [ tuple_type_p <|> int_type_p
+  , TupleType <$> (string "( " *> sepBy1 type_expression_p (string ", ") <* string " )")
+  , string "Int" *> pure IntType ]
+  :: [ Parser TupleOrIntType ]
