08c71459dbd2039e576cbac3a1e731c69238eb24
DimitrisSa <saridakisbitos@gmail.com>
2022-12-21 21:48:00 +0200

    Just Saving

--- haskell_generation/src/HaskellTypes/AfterParsing.hs
+++ haskell_generation/src/HaskellTypes/AfterParsing.hs
@@ -10,7 +10,7 @@ import HaskellTypes.Generation
   ( Stateful )
 
 import CodeGenerators.Values
-  ( base_value_type_inference_g )
+  ( base_value_g, base_value_type_inference_g )
 
 import Helpers
   ( Haskell )
@@ -36,21 +36,32 @@ combine_with_direction = ( \at1 ad at2 -> case ad of
   ) :: ApplicationTree -> ApplicationDirection -> ApplicationTree ->
        ApplicationTree
 
-application_tree_g = ( \vt at -> 
-  application_tree_type_inference_g at >>= \( inferred_vt, hs ) ->
-  case vt == inferred_vt of 
-    True -> return hs
-    False -> undefined
+application_tree_g = ( \vt@(AbsTypesAndResType abs_ts res_t) -> \case 
+  Application at1 at2 -> 
+    application_tree_type_inference_g at2 >>=
+      \( vt2, hs2 ) ->
+    application_tree_g (AbsTypesAndResType (vt_to_bt vt2 : abs_ts) res_t) at1 >>=
+      \hs1 ->
+    return $ "(" ++ hs1 ++ " " ++ hs2 ++ ")"
+  BaseValueLeaf bv -> base_value_g vt bv
   ) :: ValueType -> ApplicationTree -> Stateful Haskell
+
+vt_to_bt = undefined
+bt_to_vt = undefined
 
 application_tree_type_inference_g = ( \case 
   Application at1 at2 -> 
-    application_tree_type_inference_g at1 >>= \( vt1, hs1 ) ->
-    application_tree_type_inference_g at2 >>= \( vt2, hs2 ) ->
-    return_type vt1 vt2 >>= \ret_vt ->
-    return ( ret_vt, "(" ++ hs1 ++ " " ++ hs2 ++ ")" ) 
+    application_tree_type_inference_g at1 >>=
+      \( vt1@(AbsTypesAndResType abs_ts res_t), hs1 ) -> case abs_ts of 
+    [] -> undefined
+    abs_t:rest -> 
+      application_tree_g (bt_to_vt abs_t) at2 >>= \hs2 ->
+      return ( (AbsTypesAndResType rest res_t), "(" ++ hs1 ++ " " ++ hs2 ++ ")" ) 
   BaseValueLeaf bv -> base_value_type_inference_g bv
   ) :: ApplicationTree -> Stateful ( ValueType, Haskell )
 
-return_type = ( \vt1 vt2 -> undefined
+valid_types = ( \vt1 vt2 res_vt -> undefined
+  ) :: ValueType -> ValueType -> ValueType -> Stateful Bool
+
+infer_valid_return_type = ( \vt1 vt2 -> undefined
   ) :: ValueType -> ValueType -> Stateful ValueType
