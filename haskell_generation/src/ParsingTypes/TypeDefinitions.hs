f48e25ac24644c4db7627c1a3b5b3c94dff140b7
DimitrisSa <saridakisbitos@gmail.com>
2023-04-26 18:36:06 +0300

    ParsingTypes refactoring done

--- haskell_generation/src/ParsingTypes/TypeDefinitions.hs
+++ haskell_generation/src/ParsingTypes/TypeDefinitions.hs
@@ -12,41 +12,19 @@ import ParsingTypes.Types (TypeName, ValueType)
 -- TypeConsAndVars,
 -- Field, TupleTypeDef, OrTypeCase, OrTypeDef, TypeDefinition
 
+-- ManyTNamesInParen
+
 data ManyTNamesInParen =
   ParenTypeNames TypeName TypeName [ TypeName ]
-
-data LeftTypeVars = 
-  NoLeftTypeVars | OneLeftTypeVar TypeName | ManyLeftTypeVars ManyTNamesInParen
-
-data RightTypeVars = 
-  NoRightTypeVars | OneRightTypeVar TypeName | ManyRightTypeVars ManyTNamesInParen
-
-data TypeConsAndVars =
-  TypeConsAndVars TypeName LeftTypeVars RightTypeVars
-
-data Field =
-  NameAndType ValueName ValueType 
-
-data TupleTypeDef =
-  ConsVarsAndFields TypeConsAndVars [ Field ]
-
-data OrTypeCase =
-  NameAndMaybeInT ValueName (Maybe ValueType)
-
-data OrTypeDef =
-  ConsVarsAndCases TypeConsAndVars OrTypeCase OrTypeCase [ OrTypeCase ]
-
-data TypeDefinition =
-  TupleTypeDef TupleTypeDef | OrTypeDef OrTypeDef
-
--- Show instances:
--- ManyTNamesInParen, LeftTypeVars, RightTypeVars,
--- TypeConsAndVars,
--- Field, TupleTypeDef, OrTypeCase, OrTypeDef, TypeDefinition
 
 instance Show ManyTNamesInParen where
   show = \(ParenTypeNames tn1 tn2 tns) ->
       "(" ++ (tn1 : tn2 : tns)==>map show==>intercalate ", " ++ ")"
+
+-- LeftTypeVars
+
+data LeftTypeVars = 
+  NoLeftTypeVars | OneLeftTypeVar TypeName | ManyLeftTypeVars ManyTNamesInParen
 
 instance Show LeftTypeVars where
   show = \case
@@ -54,24 +32,49 @@ instance Show LeftTypeVars where
     OneLeftTypeVar type_name -> show type_name ++ "==>"
     ManyLeftTypeVars t_names_in_paren -> show t_names_in_paren ++ "==>"
 
+-- RightTypeVars
+
+data RightTypeVars = 
+  NoRightTypeVars | OneRightTypeVar TypeName | ManyRightTypeVars ManyTNamesInParen
+
 instance Show RightTypeVars where
   show = \case
     NoRightTypeVars -> ""
     OneRightTypeVar type_name -> "<==" ++ show type_name
     ManyRightTypeVars t_names_in_paren -> "<==" ++ show t_names_in_paren
 
+-- TypeConsAndVars
+
+data TypeConsAndVars =
+  TypeConsAndVars TypeName LeftTypeVars RightTypeVars
+
 instance Show TypeConsAndVars where
   show = \(TypeConsAndVars type_name left_type_vars right_type_vars) ->
     show left_type_vars ++ show type_name ++ show right_type_vars
+
+-- Field
+
+data Field =
+  NameAndType ValueName ValueType 
 
 instance Show Field where
   show = \(NameAndType field_name field_type) ->
     show field_name ++ ": " ++ show field_type
 
+-- TupleTypeDef
+
+data TupleTypeDef =
+  ConsVarsAndFields TypeConsAndVars [ Field ]
+
 instance Show TupleTypeDef where
   show = \(ConsVarsAndFields type_cons_and_vars fields) ->
     "\ntuple_type " ++ show type_cons_and_vars ++
     "\nvalue (" ++ fields==>map show==>intercalate ", "  ++ ")\n"
+
+-- OrTypeCase
+
+data OrTypeCase =
+  NameAndMaybeInT ValueName (Maybe ValueType)
 
 instance Show OrTypeCase where
   show = \(NameAndMaybeInT case_name maybe_case_type) ->
@@ -79,12 +82,23 @@ instance Show OrTypeCase where
       Just case_type -> "<==(value: " ++ show case_type ++ ")"
       Nothing -> ""
 
+-- OrTypeDef
+
+data OrTypeDef =
+  ConsVarsAndCases TypeConsAndVars OrTypeCase OrTypeCase [ OrTypeCase ]
+
 instance Show OrTypeDef where
   show = \(ConsVarsAndCases type_cons_and_vars case1 case2 cases) ->
     "\nor_type " ++ show type_cons_and_vars ++
     "\nvalues " ++ (case1 : case2 : cases)==>map show==>intercalate " | " ++ "\n"
 
+-- TypeDefinition
+
+data TypeDefinition =
+  TupleTypeDef TupleTypeDef | OrTypeDef OrTypeDef
+
 instance Show TypeDefinition where
   show = \case
     TupleTypeDef tuple_type_def -> show tuple_type_def   
     OrTypeDef or_type_def -> show or_type_def
+
