88de114451c458ef90e6b17ef3cc8165724fd17f
DimitrisSa <saridakisbitos@gmail.com>
2023-05-10 18:15:22 +0300

    equiv_type_apps

--- haskell_generation/src/GenerationHelpers/TypeChecking.hs
+++ haskell_generation/src/GenerationHelpers/TypeChecking.hs
@@ -15,10 +15,10 @@ import GenerationState.TypesAndOperations (Stateful, type_map_get)
 -- All: equiv_types, equiv_func_types, equiv_prod_types,
 
 equiv_types = ( \val_type1 val_type2 -> case (val_type1, val_type2) of
-  (TypeApp (ConsAndTIns t_name1 _), TypeApp (ConsAndTIns t_name2 _)) ->
-    return $ t_name1 == t_name2
-  (ProdType (ProdTypes types1), ProdType (ProdTypes types2)) ->
-    equiv_prod_types types1 types2
+  (TypeApp type_app1, TypeApp type_app2) ->
+    equiv_type_apps type_app1 type_app2
+  (ProdType prod_type1, ProdType prod_type2) ->
+    equiv_prod_types prod_type1 prod_type2
   (FuncType func_type1, FuncType func_type2) ->
     equiv_func_types func_type1 func_type2
   _ -> return False
@@ -30,7 +30,12 @@ equiv_func_types = ( \(InAndOutTs in_t1 out_t1) (InAndOutTs in_t2 out_t2) ->
   return $ equiv_in_ts && equiv_out_ts
   ) :: FuncType -> FuncType -> Stateful Bool
 
-equiv_prod_types = ( \val_ts1 val_ts2 -> case length val_ts1 == length val_ts2 of
-  True -> zipWithM equiv_types val_ts1 val_ts2==>fmap and
-  False -> return False
-  ) :: [ ValType ] -> [ ValType ] -> Stateful Bool
+equiv_prod_types = ( \(ProdTypes val_ts1) (ProdTypes val_ts2) ->
+  case length val_ts1 == length val_ts2 of
+    True -> zipWithM equiv_types val_ts1 val_ts2==>fmap and
+    False -> return False
+  ) :: ProdType -> ProdType -> Stateful Bool
+
+equiv_type_apps = ( \(ConsAndTIns t_name1 in_ts1) (ConsAndTIns t_name2 in_ts2) ->
+  zipWithM equiv_types in_ts1 in_ts2==>fmap and==>fmap (&& (t_name1 == t_name2))
+  ) :: TypeApp -> TypeApp -> Stateful Bool
