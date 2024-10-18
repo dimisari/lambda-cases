6ea2f3b92ada766878d9fd0d8328ff6e9111412d
DS <saridakisbitos@gmail.com>
2024-10-19 00:39:06 +0300

    fixed test/outputs git ignore

--- test/outputs/grammar_rules/big_list.hs
+++
@@ -1,5 +0,0 @@
-[ \x -> print'("I'm the first function and x + 1 is: " !+ (x !+ (1 :: P.Integer)))
-, \x -> print'("I'm the second function and x + 2 is: " !+ (x !+ (2 :: P.Integer)))
-, \x -> print'("I'm the third function and x + 3 is: " !+ (x !+ (3 :: P.Integer)))
-]
-
